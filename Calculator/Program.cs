8e91699eca3df5d90e15f9484d5109aad1d45897
Iago Soares <iago.jt21@gmail.com>
2021-09-28 18:43:43 -0300

    primeiro commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+
+
+
+        }
+
+        static void soma()
+        {
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+
+            imprimir(resultado);           
+
+        }
+
+        static void subtracao()
+        {
+
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+            imprimir(resultado);      
+
+        }
+
+        static void multiplicacao()
+        {
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+
+            imprimir(resultado);      
+
+        }
+
+        static void divisao()
+        {
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            if(v2 == 0){
+                Console.WriteLine("Não é possivel dividir por zero !!");
+                Menu();  
+            }else{
+
+                float resultado = v1 / v2;
+                imprimir(resultado);
+                 
+                
+            }
+
+                
+
+        }
+
+        static void imprimir(float res){
+            Console.WriteLine(res);
+            Menu();  
+        }
+
+        static void Menu()
+        {
+
+            
+            Console.WriteLine("### Escolha uma operação matematica ###");
+
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Sair da Aplicação");
+
+
+            Console.WriteLine("-------------------------");
+            short n = short.Parse(Console.ReadLine());
+
+            switch(n){
+                case 1: soma(); break;
+                case 2: subtracao(); break;
+                case 3: multiplicacao(); break;
+                case 4: divisao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ timewatch/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace timewatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Menu();
+        }
+
+        static void start(int time){
+            
+            int currentTime = 0;
+
+            while(currentTime != time){
+
+                Console.Clear();
+                currentTime ++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+
+            }
+            
+            for(int i = 0; i < 3; i++){
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado, Retornando para o menu !");
+            Thread.Sleep(1000);
+            Console.Clear();            
+            Console.WriteLine("Stopwatch finalizado, Retornando para o menu !!");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado, Retornando para o menu !!!");
+            Thread.Sleep(1000);
+            }
+
+            Menu();
+            
+            
+        }
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("S - Seconds");
+            Console.WriteLine("M - Minutes");
+            Console.WriteLine("E - Exit");
+            Console.WriteLine("--------------------");
+
+                        
+            char v = char.Parse(Console.ReadLine().ToLower());
+
+            switch (v){
+                case 's': Segundos(); break;
+                case 'm': Minutos(); break;
+                case 'e': System.Environment.Exit(0); break;
+                default: TratamentoErro(); break; 
+            }
+        }
+
+        static void Segundos(){
+            
+            Console.WriteLine("Quantos segundos ?");
+            int time = int.Parse(Console.ReadLine());            
+
+        }
+
+        static void Minutos(){
+            
+            Console.WriteLine("Quantos minutos ?");
+            int time = int.Parse(Console.ReadLine());
+            time = time * 60;            
+            start(time);
+
+        }
+
+        static void TratamentoErro(){
+            Console.WriteLine("Digite um opção valida !");
+            Thread.Sleep(1000);
+            Menu();
+
+        }
+    }
+}
