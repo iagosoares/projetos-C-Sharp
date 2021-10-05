b8d8180bbc70659d290897a541ee9ed12b815304
Iago Soares <iago.jt21@gmail.com>
2021-10-04 21:27:35 -0300

    first commit

---
+++ TextEditor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu(){
+            Console.Clear();
+            Console.WriteLine("Escolha uma opção:");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair da aplicação");
+            Console.WriteLine("----------------------");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch(option){}
+
+        }
+    }
+}
